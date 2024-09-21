a113d2030d394e5d36b4e7408624ba4dea5ddfbf
Markangelohil <paderomarkangelohil_bscs@plmun.edu.ph>
2024-09-21 09:27:03 +0800

    Add files via upload

---
+++ grp4_Padero_ex2.py
@@ -0,0 +1,38 @@
+# Arithmetic Operations using two numbers
+num1 = 10  # You can change this to a float or complex number
+num2 = 5   # You can change this to a float or complex number
+
+# Performing operations
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+modulus = num1 % num2
+exponent = num1 ** num2
+
+# Displaying results
+print("Addition:", addition)
+print("Subtraction:", subtraction)
+print("Multiplication:", multiplication)
+print("Division:", division)
+print("Modulus:", modulus)
+print("Exponent:", exponent)
+
+# Creating two variables with the same integer literal
+num1 = 25_000_000
+num2 = 25000000
+
+# Printing the values
+print(num1)
+print(num2)
+
+
+# Creating variables
+integer_var = 10
+float_var = 20.5
+complex_var = 2 + 3j
+
+# Checking the types
+print("Type of integer_var:", type(integer_var))
+print("Type of float_var:", type(float_var))
+print("Type of complex_var:", type(complex_var))
