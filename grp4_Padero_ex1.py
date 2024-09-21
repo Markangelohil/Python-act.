07cf7adbcff0bbc3717a1e894d1b1d917f53807d
Markangelohil <paderomarkangelohil_bscs@plmun.edu.ph>
2024-09-21 09:05:06 +0800

    Add files via upload

---
+++ grp4_Padero_ex1.py
@@ -0,0 +1,25 @@
+#My first simple script
+print ("Hello,I,m Mark Angelohil Padero! BSCS 3F")
+age = 22
+name = "Mark"
+
+print("Age: ",age)
+print("Name: ",name)
+
+hadje = "hadje"
+rafael = "rafael"
+rochelle = "rochelle"
+cylen = "cylen"
+
+print (hadje)
+print (rafael)
+print (rochelle)
+print (cylen)
+
+age,name,gender = 22 , "Mark" ,"male"
+print ("Age:",age)
+print ("Name:",name)
+print ("Gender:",gender)
+
+age,edad,num = 22 ,22, 22
+print(age,edad,num)
